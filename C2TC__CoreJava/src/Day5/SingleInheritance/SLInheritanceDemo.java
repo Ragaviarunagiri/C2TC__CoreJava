6f42e3096b8c1cc96ebc795f1ddd0573fcf2140f
ADMIN <ADMIN@RAGAVIarunagiri>
2025-07-25 00:43:21 +0530

    Day4

---
+++ C2TC__CoreJava/src/Day5/SingleInheritance/SLInheritanceDemo.java
@@ -0,0 +1,17 @@
+
+	//Program to demonstrate Single Inheritance 
+
+	package Day5.SingleInheritance;
+
+	public class SLInheritanceDemo {
+	public static void main(String[] args) {
+			
+			Citizen c = new Citizen("Jinu", "122921292931", "Seoul", 9159186845L);
+			System.out.println(c);
+			
+			Student student = new Student("Rumi", "232929112903", "Busan", 9888878787L, 27, "Busan University");
+			System.out.println(student);
+		}
+
+	
+}
